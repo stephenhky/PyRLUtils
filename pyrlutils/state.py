d7dcbec33e3768cba5eefffb9f01b609829c34fc
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-06-17 22:56:21 -0400

    updated discrete test

--- pyrlutils/state.py
+++ pyrlutils/state.py
@@ -1,6 +1,6 @@
 
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 
@@ -10,9 +10,26 @@ class State(ABC):
 
 
 class DiscreteState(State):
-    @abstractmethod
+    def __init__(self, all_state_values: List, initial_values: Optional[List] = None):
+        super().__init__()
+        self._all_state_values = all_state_values
+        self._state_value = initial_values if initial_values is not None and initial_values in self._all_state_values else self._all_state_values[0]
+
+    def get_state_value(self):
+        return self._state_value
+
+    @property
+    def state_value(self):
+        return self.get_state_value()
+
+    def set_state_value(self, state_value):
+        if state_value in self._all_state_values:
+            self._state_value = state_value
+        else:
+            raise ValueError('State value {} is invalid.'.format(state_value))
+
     def get_all_possible_state_values(self) -> List:
-        raise NotImplementedError()
+        return self.all_state_values
 
 
 class InvalidRangeError(Exception):
