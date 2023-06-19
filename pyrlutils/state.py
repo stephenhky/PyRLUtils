94ca0d5966bc86f41c2a4dee3255024d15bd06c6
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-06-19 16:57:52 -0400

    continous state, and abstract class

--- pyrlutils/state.py
+++ pyrlutils/state.py
@@ -6,7 +6,13 @@ import numpy as np
 
 
 class State(ABC):
-    pass
+    @abstractmethod
+    def set_state_value(self, state_value):
+        pass
+
+    @abstractmethod
+    def get_state_value(self):
+        pass
 
 
 class DiscreteState(State):
@@ -39,8 +45,8 @@ class InvalidRangeError(Exception):
 
 
 class ContinuousState(State):
-    def __init__(self, nbdims: int, ranges: List[np.array]):
-        self.nbdims = nbdims
+    def __init__(self, nbdims: int, ranges: List[np.array], init_value: np.ndarray = None):
+        self._nbdims = nbdims
 
         try:
             assert (ranges.dtype == np.float64) or (ranges.dtype == np.float32) or (ranges.dtype == np.float16)
@@ -48,7 +54,7 @@ class ContinuousState(State):
             raise TypeError('It has to be floating type numpy.ndarray.')
 
         try:
-            assert self.nbdims == ranges.shape[0]
+            assert self._nbdims == ranges.shape[0]
         except AssertionError:
             raise ValueError('Number of ranges does not meet the number of dimensions.')
 
@@ -58,7 +64,49 @@ class ContinuousState(State):
         except AssertionError:
             raise InvalidRangeError()
 
-        self.ranges = ranges
+        self._ranges = ranges
+        if init_value is None:
+            self._state_value = np.zeros(self._nbdims)
+            for i in range(self._nbdims):
+                self._state_value[i] = np.random.uniform(self._ranges[i, 0], self._ranges[i, 1])
+        else:
+            try:
+                assert init_value.shape[0] == self._nbdims
+            except AssertionError:
+                raise ValueError('Initialized value does not have the right dimension.')
+            for i in range(self._nbdims):
+                try:
+                    assert (init_value[i] >= self._ranges[i, 0]) and (init_value[i] <= self.ranges[i, 1])
+                except AssertionError:
+                    raise InvalidRangeError('Initialized value at dimension {} (value: {}) is not within the permitted range ({} -> {})!'.format(i, init_value[i], self._ranges[i, 0], self._ranges[i, 1]))
+            self._state_value = init_value
 
-    def get_state_value_ranges(self):
-        return self.ranges
+    def set_state_value(self, state_value: np.ndarray):
+        try:
+            assert state_value.shape[0] == self._nbdims
+        except AssertionError:
+            raise ValueError('Given value does not have the right dimension.')
+        self._state_value = state_value
+
+    def get_state_value(self) -> np.ndarray:
+        return self._state_value
+
+    def get_state_value_ranges(self) -> np.ndarray:
+        return self._ranges
+
+    def get_state_value_range_at_dimension(self, dimension: int) -> np.ndarray:
+        return self._ranges[dimension]
+
+    @property
+    def ranges(self) -> np.ndarray:
+        return self.get_state_value_ranges()
+
+    @property
+    def state_value(self) -> np.ndarray:
+        return self.get_state_value()
+
+    @property
+    def nbdims(self) -> int:
+        return self._nbdims
+
+
