cfa1882ffac1012549796c2fa4fc78643b71516b
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-07-15 17:39:27 -0400

    type casting

--- pyrlutils/action.py
+++ pyrlutils/action.py
@@ -1,9 +1,11 @@
 
+from types import LambdaType
+
 from .state import State
 
 
 class Action:
-    def __init__(self, actionfunc):
+    def __init__(self, actionfunc: LambdaType):
         self._actionfunc = actionfunc
 
     def act(self, state: State) -> State:
