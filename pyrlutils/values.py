256e138814ff8ef4764bb8f22eb5b5656b12f226
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-07-16 16:45:10 -0400

    implemented something about reward function

--- pyrlutils/values.py
+++ pyrlutils/values.py
@@ -1,6 +1,9 @@
 
 from abc import ABC, abstractmethod
 
+from .state import State
+from .action import Action
+
 
 class IndividualRewardFunction(ABC):
     @abstractmethod
@@ -9,3 +12,24 @@ class IndividualRewardFunction(ABC):
 
     def __call__(self, state_value, action_value, next_state_value) -> float:
         return self.reward(state_value, action_value, next_state_value)
+
+
+class RewardFunction(ABC):
+    def __init__(self, discount_factor: float, individual_reward_function: IndividualRewardFunction):
+        self._discount_factor = discount_factor
+        self._individual_reward_function = individual_reward_function
+
+    @property
+    def discount_factor(self) -> float:
+        return self._discount_factor
+
+    @discount_factor.setter
+    def discount_factor(self, discount_factor):
+        self._discount_factor = discount_factor
+
+    def individual_reward(self, state_value, action_value, next_state_value) -> float:
+        return self._individual_reward_function(state_value, action_value, next_state_value)
+
+    @abstractmethod
+    def total_reward(self, state: State, action: Action):
+        pass
