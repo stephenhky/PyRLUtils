fbb34535127fdf53bd2964c23fea7d38026835b5
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-07-28 18:52:34 -0400

    added args and kwargs for action

--- pyrlutils/action.py
+++ pyrlutils/action.py
@@ -8,8 +8,8 @@ class Action:
     def __init__(self, actionfunc: LambdaType):
         self._actionfunc = actionfunc
 
-    def act(self, state: State) -> State:
-        self._actionfunc(state)
+    def act(self, state: State, *args, **kwargs) -> State:
+        self._actionfunc(state, *args, **kwargs)
         return state
 
     def __call__(self, state: State) -> State:
