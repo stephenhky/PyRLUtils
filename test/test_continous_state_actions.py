9be06d6ba027e764ad9add0631023c13a3e0da8a
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-07-30 16:58:26 -0400

    fixed mistake in unit test itself

--- test/test_continous_state_actions.py
+++ test/test_continous_state_actions.py
@@ -58,9 +58,9 @@ class TestContinuousSystem(unittest.TestCase):
         actions['left'](state, 2.0)
         np.testing.assert_array_almost_equal(state.state_value, np.array([-0.5, 0.0]))
         actions['up'](state, 1.0)
-        np.testing.assert_array_almost_equal(state.state_value, np.array([1.5, 1.0]))
+        np.testing.assert_array_almost_equal(state.state_value, np.array([-0.5, 1.0]))
         actions['down'](state, 0.7)
-        np.testing.assert_array_almost_equal(state.state_value, np.array([1.5, 0.3]))
+        np.testing.assert_array_almost_equal(state.state_value, np.array([-0.5, 0.3]))
 
 
 if __name__ == '__main__':
