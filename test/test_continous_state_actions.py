454b9bf8ee32f88861e2e842a44dba5644c8c9b0
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-07-30 15:18:54 -0400

    rounding off error fixed

--- test/test_continous_state_actions.py
+++ test/test_continous_state_actions.py
@@ -14,23 +14,24 @@ class TestContinuousSystem(unittest.TestCase):
             'decrease': lambda s, v: s.set_state_value(s.state_value-v if s.state_value > -1.5+v else -1.5)
         }
 
-        assert state.state_value == 0.0
+        assert round(state.state_value, 1) == 0.0
         actions['increase'](state, 0.2)
-        assert state.state_value == 0.2
+        assert round(state.state_value, 1) == 0.2
         actions['increase'](state, 1.0)
-        assert state.state_value == 1.2
+        assert round(state.state_value, 1) == 1.2
         actions['increase'](state, 1.0)
-        assert state.state_value == 1.5
+        assert round(state.state_value, 1) == 1.5
         actions['decrease'](state, 0.4)
-        assert state.state_value == 1.1
+        assert round(state.state_value, 1) == 1.1
         actions['decrease'](state, 2.0)
-        assert state.state_value == -0.9
+        print(state.state_value)
+        assert round(state.state_value, 1) == -0.9
         actions['decrease'](state, 1.0)
-        assert state.state_value == -1.5
+        assert round(state.state_value, 1) == -1.5
         actions['increase'](state, 0.5)
-        assert state.state_value == -1.0
+        assert round(state.state_value, 1) == -1.0
         state.state_value = 0.0
-        assert state.state_value == 0.0
+        assert round(state.state_value, 1) == 0.0
 
 
 if __name__ == '__main__':
