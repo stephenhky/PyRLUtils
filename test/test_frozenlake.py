be05fa5cac80404cc7a1cf7a479676d2fe0b6221
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-08-05 00:25:25 -0400

    frozen lake in unit test

--- test/test_frozenlake.py
+++ test/test_frozenlake.py
@@ -9,3 +9,17 @@ class TestFrozenLake(unittest.TestCase):
         state, actions_dict, ind_reward_fcn = tranprob.generate_mdp_objects()
 
         assert len(state.get_all_possible_state_values()) == 16
+        assert state.state_value == 0
+
+        actions_dict[0](state)
+        assert state.state_value in {0, 4}
+
+        state.state_value = 15
+        actions_dict[2](state)
+        assert state.state_value == 15
+
+        assert ind_reward_fcn(state.state_value, 1, 15) == 1.
+
+
+if __name__ == '__main__':
+    unittest.main()
