73ced5dfc2e783e39c743871d3ab7ee067b8e29c
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-08-05 00:38:37 -0400

    deleted conversion function

--- pyrlutils/transition.py
+++ pyrlutils/transition.py
@@ -125,24 +125,6 @@ class TransitionProbabilityFactory:
         return state, actions_dict, individual_reward_fcn
 
 
-def convert_openai_gymenv_to_transprob(envname):
-    gymenv = gym.make(envname)
-    P = gymenv.env.P
-
-    factory = TransitionProbabilityFactory()
-    for state_value, trans_dict in P.items():
-        new_trans_dict = {}
-        for action_value, next_state_list in trans_dict.items():
-            new_trans_dict[action_value] = [
-                NextStateTuple(next_state[1], next_state[0], next_state[2], next_state[3])
-                for next_state in next_state_list
-            ]
-
-        factory.add_state_transitions(state_value, new_trans_dict)
-
-    return factory
-
-
 class OpenAIGymDiscreteEnvironmentTransitionProbabilityFactory(TransitionProbabilityFactory):
     def __init__(self, envname):
         super().__init__()
