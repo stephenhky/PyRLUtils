5a656c5a7da3edb512d9130cd284bc1ff83956e5
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-08-05 00:33:55 -0400

    conversion function to a class

--- test/test_frozenlake.py
+++ test/test_frozenlake.py
@@ -1,12 +1,12 @@
 
 import unittest
 
-from pyrlutils.transition import convert_openai_gymenv_to_transprob
+from pyrlutils.transition import OpenAIGymDiscreteEnvironmentTransitionProbabilityFactory
 
 class TestFrozenLake(unittest.TestCase):
     def test_factory(self):
-        tranprob = convert_openai_gymenv_to_transprob('FrozenLake-v1')
-        state, actions_dict, ind_reward_fcn = tranprob.generate_mdp_objects()
+        tranprobfactory = OpenAIGymDiscreteEnvironmentTransitionProbabilityFactory('FrozenLake-v1')
+        state, actions_dict, ind_reward_fcn = tranprobfactory.generate_mdp_objects()
 
         assert len(state.get_all_possible_state_values()) == 16
         assert state.state_value == 0
@@ -17,8 +17,6 @@ class TestFrozenLake(unittest.TestCase):
         state.state_value = 15
         actions_dict[2](state)
         assert state.state_value == 15
-
-        assert ind_reward_fcn(state.state_value, 1, 15) == 1.
 
 
 if __name__ == '__main__':
