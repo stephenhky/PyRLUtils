2cea855909f8c390b7b4fe4764e1188fccd215c4
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-08-07 22:35:09 -0400

    more sophiscated deterministic policy

--- pyrlutils/policy.py
+++ pyrlutils/policy.py
@@ -1,10 +1,11 @@
 
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Union, Dict
+from warnings import warn
 
 import numpy as np
 
-from .state import State
+from .state import State, DiscreteState, DiscreteStateValueType
 from .action import Action, DiscreteActionValueType
 
 
@@ -22,14 +23,35 @@ class Policy(ABC):
 
 
 class DeterministicPolicy(Policy):
+    @abstractmethod
+    def add_deterministic_rule(self, *args, **kwargs):
+        pass
+
     @property
     def is_stochastic(self) -> bool:
         return False
 
 
+class DiscreteDeterminsticPolicy(DeterministicPolicy):
+    def __init__(self, actions_dict: Dict[DiscreteActionValueType, Action]):
+        self._state_to_action = {}
+        self._actions_dict = actions_dict
+
+    def add_policy_rule(self, state_value: DiscreteStateValueType, action_value: DiscreteActionValueType):
+        if state_value in self._state_to_action:
+            warn('State value {} exists in rule; it will be replaced.'.format(state_value))
+        self._state_to_action[state_value] = action_value
+
+    def get_action_value(self, state_value: DiscreteStateValueType) -> DiscreteActionValueType:
+        return self._state_to_action.get(state_value)
+
+    def get_action(self, state: DiscreteState) -> Action:
+        return self._actions_dict[self.get_action_value(state.state_value)]
+
+
 class StochasticPolicy(Policy):
     @abstractmethod
-    def get_probability(self, state: State, action_value, *args, **kwargs) -> float:
+    def get_probability(self, *args, **kwargs) -> float:
         pass
 
     @property
@@ -39,11 +61,11 @@ class StochasticPolicy(Policy):
 
 class DiscreteStochasticPolicy(StochasticPolicy):
     @abstractmethod
-    def get_probability(self, state: State, action_value: DiscreteActionValueType) -> float:
+    def get_probability(self, state_value: DiscreteStateValueType, action_value: DiscreteActionValueType) -> float:
         pass
 
 
 class ContinuousStochasticPolicy(StochasticPolicy):
     @abstractmethod
-    def get_probability(self, state: State, action_value: DiscreteActionValueType, value: Union[float, np.ndarray]) -> float:
+    def get_probability(self, state_value: Union[float, np.ndarray], action_value: DiscreteActionValueType, value: Union[float, np.ndarray]) -> float:
         pass
