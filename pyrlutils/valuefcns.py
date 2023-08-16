e86123089183769f1e2c5d0439cb37d235e2d798
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-08-15 23:57:13 -0400

    implemented _policy_evaluation method

--- pyrlutils/valuefcns.py
+++ pyrlutils/valuefcns.py
@@ -22,18 +22,35 @@ class OptimalPolicyOnValueFunctions:
         self._evaluated = False
         self._improved = False
 
-    def _policy_evaluation(self, policy: DiscreteDeterminsticPolicy):
-        prev_V = np.zeros(len(self._states_to_indices))
-        V = np.zeros(len(self._states_to_indices))
+        self._theta = 1e-10
+        self._policy_evaluation_maxiter = 10000
 
-        for state_value in self._states_to_indices.keys():
-            state_index = self._states_to_indices[state_value]
-            action_value = policy.get_action_value(state_value)
-            for next_state_tuple in self._transprobfac[state_value][action_value]:
-                prob = next_state_tuple.probability
-                reward = next_state_tuple.reward
-                next_state_value = next_state_tuple.next_state_value
-                next_state_index = self._states_to_indices[next_state_value]
-                # be careful with the logic....
+    def _policy_evaluation(self, policy: DiscreteDeterminsticPolicy) -> np.ndarray:
+        prev_V = np.zeros(len(self._states_to_indices))
+
+        for _ in range(self._policy_evaluation_maxiter):
+            V = np.zeros(len(self._states_to_indices))
+            for state_value in self._states_to_indices.keys():
+                state_index = self._states_to_indices[state_value]
+                action_value = policy.get_action_value(state_value)
+                for next_state_tuple in self._transprobfac[state_value][action_value]:
+                    prob = next_state_tuple.probability
+                    reward = next_state_tuple.reward
+                    next_state_value = next_state_tuple.next_state_value
+                    next_state_index = self._states_to_indices[next_state_value]
+                    terminal = next_state_tuple.terminal
+
+                    V[state_index] += prob * (reward + self._gamma*prev_V[next_state_index] if not terminal else 0.)
+
+            if np.max(np.abs(V-prev_V)) < self._theta:
+                break
+
+            prev_V = V.copy()
+
+        return V
+
+    def _policy_improvement(self, V: np.ndarray, policy: DiscreteDeterminsticPolicy):
+        pass
+
 
 
