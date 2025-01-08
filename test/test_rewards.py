e2fb7c6e2af2d2aa7531c06adf5ac756d66c8017
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-01-08 00:26:25 -0500

    try constructing reward function

--- test/test_rewards.py
+++ test/test_rewards.py
@@ -1,10 +1,13 @@
 
 import unittest
 
+from pyrlutils.openai.utils import OpenAIGymDiscreteEnvironmentTransitionProbabilityFactory
+
 
 class TestRewards(unittest.TestCase):
     def test_reward1(self):
-        pass
+        tranprobfactory = OpenAIGymDiscreteEnvironmentTransitionProbabilityFactory('FrozenLake-v1')
+        state, actions_dict, ind_reward_fcn = tranprobfactory.generate_mdp_objects()
 
 
 if __name__ == '__main__':
