4825697cdef7978f0e59ec036762fc7a005c5ecc
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-08-31 23:32:34 -0400

    correct hashcode again

--- test/test_state.py
+++ test/test_state.py
@@ -55,8 +55,8 @@ class TestState(unittest.TestCase):
         assert state.encode_coordinates([-5, -1]) == 22
         assert state.encode_coordinates([-5, 0]) == 33
         assert state.encode_coordinates([-5, 1]) == 44
-        assert state.encode_coordinates([0, 1]) == 50
-        assert state.decode_coordinates(49) == [0, 0]
+        assert state.encode_coordinates([0, 1]) == 49
+        assert state.decode_coordinates(48) == [0, 0]
 
 
 
