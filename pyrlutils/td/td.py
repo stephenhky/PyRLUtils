e8b3b40cccabb9c8e1f6ba8ea779dddf7949fdb9
stephenhky <stephenhky@yahoo.com.hk>
2025-09-03 17:06:02 -0400

    basic implementation of temporal difference

--- pyrlutils/td/td.py
+++ pyrlutils/td/td.py
@@ -1,4 +1,6 @@
 
+from typing import Optional
+
 import numpy as np
 
 from ..policy import DiscretePolicy
@@ -10,12 +12,12 @@ class TDLearner:
     def __init__(
             self,
             transprobfac: TransitionProbabilityFactory,
-            gamma: float = 1.0,
-            init_alpha: float = 0.5,
-            min_alpha: float = 0.01,
+            gamma: float=1.0,
+            init_alpha: float=0.5,
+            min_alpha: float=0.01,
             alpha_decay_ratio: float=0.3,
-            policy: DiscretePolicy = None,
-            initial_state_index: int = 0
+            policy: Optional[DiscretePolicy]=None,
+            initial_state_index: int=0
     ):
         self._gamma = gamma
         self._init_alpha = init_alpha
@@ -27,13 +29,11 @@ class TDLearner:
         self._alpha_decay_ratio = alpha_decay_ratio
         self._transprobfac = transprobfac
         self._state, self._actions_dict, self._indrewardfcn = self._transprobfac.generate_mdp_objects()
-        self._state_names = self._state.get_all_possible_state_values()
-        self._states_to_indices = {state: idx for idx, state in enumerate(self._state_names)}
         self._action_names = list(self._actions_dict.keys())
         self._actions_to_indices = {action_value: idx for idx, action_value in enumerate(self._action_names)}
         self._policy = policy
         try:
-            assert 0 <= initial_state_index < len(self._states_names)
+            assert 0 <= initial_state_index < self._state.nb_state_values
         except AssertionError:
             raise ValueError("Initial state index must be between 0 and {}".format(len(self._state_names)))
         self._init_state_index = initial_state_index
@@ -49,11 +49,27 @@ class TDLearner:
             self._state.set_state_value(self._init_state_index)
             done = False
             while not done:
+                this_state_index = self._state.state_index
                 action_value = self._policy.get_action_value(self._state)
+                action_func = self._actions_dict[action_value]
+                self._state = action_func(self._state)
+                new_state_index = self._state.state_index
+                reward = self._indrewardfcn(
+                    self._state._get_state_value_from_index(this_state_index),
+                    action_value,
+                    self._state._get_state_value_from_index(new_state_index)
+                )
+                done = self._state.is_terminal
+
+                td_target = reward + self._gamma * V[new_state_index] * (not done)
+                td_error = td_target - V[this_state_index]
+                V[this_state_index] = V[this_state_index] + alphas[i] * td_error
+
+            V_track[i, :] = V
 
     @property
     def nb_states(self) -> int:
-        return len(self._states_names)
+        return self._state.nb_state_values
 
     @property
     def policy(self) -> DiscretePolicy:
