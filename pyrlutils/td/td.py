7ff561d743dc9f64f10b442b4c2cc84113808eeb
stephenhky <stephenhky@yahoo.com.hk>
2025-09-19 12:08:31 -0400

    modularized

--- pyrlutils/td/td.py
+++ pyrlutils/td/td.py
@@ -1,44 +1,11 @@
 
-from typing import Optional
-
 import numpy as np
 from nptyping import NDArray, Shape, Float
 
-from ..policy import DiscretePolicy
-from ..transition import TransitionProbabilityFactory
-from .utils import decay_schedule
+from .utils import decay_schedule, AbstractTemporalDifferenceLearner
 
 
-class TDLearner:
-    def __init__(
-            self,
-            transprobfac: TransitionProbabilityFactory,
-            gamma: float=1.0,
-            init_alpha: float=0.5,
-            min_alpha: float=0.01,
-            alpha_decay_ratio: float=0.3,
-            policy: Optional[DiscretePolicy]=None,
-            initial_state_index: int=0
-    ):
-        self._gamma = gamma
-        self._init_alpha = init_alpha
-        self._min_alpha = min_alpha
-        try:
-            assert 0.0 <= alpha_decay_ratio <= 1.0
-        except AssertionError:
-            raise ValueError("alpha_decay_ratio must be between 0 and 1!")
-        self._alpha_decay_ratio = alpha_decay_ratio
-        self._transprobfac = transprobfac
-        self._state, self._actions_dict, self._indrewardfcn = self._transprobfac.generate_mdp_objects()
-        self._action_names = list(self._actions_dict.keys())
-        self._actions_to_indices = {action_value: idx for idx, action_value in enumerate(self._action_names)}
-        self._policy = policy
-        try:
-            assert 0 <= initial_state_index < self._state.nb_state_values
-        except AssertionError:
-            raise ValueError("Initial state index must be between 0 and {}".format(len(self._state_names)))
-        self._init_state_index = initial_state_index
-
+class TemporalDifferenceLearner(AbstractTemporalDifferenceLearner):
     def learn(self, episodes: int) -> tuple[NDArray[Shape["*"], Float], NDArray[Shape["*, *"], Float]]:
         V = np.zeros(self.nb_states)
         V_track = np.zeros((episodes, self.nb_states))
@@ -67,51 +34,3 @@ class TDLearner:
             V_track[i, :] = V
 
         return V, V_track
-
-    @property
-    def nb_states(self) -> int:
-        return self._state.nb_state_values
-
-    @property
-    def policy(self) -> DiscretePolicy:
-        return self._policy
-
-    @policy.setter
-    def policy(self, val: DiscretePolicy):
-        self._policy = val
-
-    @property
-    def gamma(self) -> float:
-        return self._gamma
-
-    @gamma.setter
-    def gamma(self, val: float):
-        self._gamma = val
-
-    @property
-    def init_alpha(self) -> float:
-        return self._init_alpha
-
-    @init_alpha.setter
-    def init_alpha(self, val: float):
-        self._init_alpha = val
-
-    @property
-    def min_alpha(self) -> float:
-        return self._min_alpha
-
-    @min_alpha.setter
-    def min_alpha(self, val: float):
-        self._min_alpha = val
-
-    @property
-    def alpha_decay_ratio(self) -> float:
-        return self._alpha_decay_ratio
-
-    @property
-    def initial_state_index(self) -> int:
-        return self._init_state_index
-
-    @initial_state_index.setter
-    def initial_state_index(self, val: int):
-        self._init_state_index = val
