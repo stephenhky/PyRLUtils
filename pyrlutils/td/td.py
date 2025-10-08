973bdbe46a05c7d969f4a03c522d8e39e2f147d6
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-10-08 11:47:11 -0400

    comments removed

--- pyrlutils/td/td.py
+++ pyrlutils/td/td.py
@@ -11,10 +11,7 @@ class SingleStepTemporalDifferenceLearner(AbstractTemporalDifferenceLearner):
     def learn(
             self,
             episodes: int
-    # ) -> tuple[Annotated[NDArray[np.float64], "1D Array"], Annotated[NDArray[np.float64], "2D Array"]]:
     ) -> tuple[Annotated[NumpyNDArrayWrappedDict, "1D Array"], Annotated[NumpyNDArrayWrappedDict, "2D Array"]]:
-        # V = np.zeros(self.nb_states)
-        # V_track = np.zeros((episodes, self.nb_states))
         V = NumpyNDArrayWrappedDict(
             [self._state.get_all_possible_state_values()],
             default_initial_value=0.0
@@ -32,24 +29,18 @@ class SingleStepTemporalDifferenceLearner(AbstractTemporalDifferenceLearner):
             self._state.state_index = self.initial_state_index
             done = False
             while not done:
-                # old_state_index = self._state.state_index
                 old_state_value = self._state.state_value
                 action_value = self._policy.get_action_value(self._state.state_value)
                 action_func = self._actions_dict[action_value]
                 self._state = action_func(self._state)
-                # new_state_index = self._state.state_index
                 new_state_value = self._state.state_value
                 reward = self._indrewardfcn(old_state_value, action_value, new_state_value)
                 done = self._state.is_terminal
 
-                # td_target = reward + self.gamma * V[new_state_index] * (not done)
                 td_target = reward + self.gamma * V[new_state_value] * (not done)
-                # td_error = td_target - V[old_state_index]
                 td_error = td_target - V[old_state_value]
-                # V[old_state_index] = V[old_state_index] + alphas[i] * td_error
                 V[(old_state_value,)] = V[old_state_value] + alphas[i] * td_error
 
-            # V_track[i, :] = V
             V_track_array[i, :] = V_array
 
         return V, V_track
@@ -60,10 +51,7 @@ class MultipleStepTemporalDifferenceLearner(AbstractTemporalDifferenceLearner):
             self,
             episodes: int,
             n_steps: int=3
-    # ) -> tuple[Annotated[NDArray[np.float64], "1D Array"], Annotated[NDArray[np.float64], "2D Array"]]:
     ) -> tuple[Annotated[NumpyNDArrayWrappedDict, "1D Array"], Annotated[NumpyNDArrayWrappedDict, "2D Array"]]:
-        # V = np.zeros(self.nb_states)
-        # V_track = np.zeros((episodes, self.nb_states))
         V = NumpyNDArrayWrappedDict(
             [self._state.get_all_possible_state_values()],
             default_initial_value=0.0
@@ -86,14 +74,12 @@ class MultipleStepTemporalDifferenceLearner(AbstractTemporalDifferenceLearner):
             while not done or path is not None:
                 path = path[1:]     # worth revisiting this line
 
-                next_state_index = -1
+                new_state_value = self._state._get_state_value_from_index(self._state.nb_state_values-1)
                 while not done and len(path) < n_steps:
-                    # old_state_index = self._state.state_index
                     old_state_value = self._state.state_value
                     action_value = self._policy.get_action_value(self._state.state_value)
                     action_func = self._actions_dict[action_value]
                     self._state = action_func(self._state)
-                    # new_state_index = self._state.state_index
                     new_state_value = self._state.state_value
                     reward = self._indrewardfcn(old_state_value, action_value, new_state_value)
                     done = self._state.is_terminal
@@ -120,7 +106,6 @@ class MultipleStepTemporalDifferenceLearner(AbstractTemporalDifferenceLearner):
                 if len(path) == 1 and path[0].done:
                     path = None
 
-            # V_track[i, :] = V
             V_track_array[i, :] = V_array
 
         return V, V_track
